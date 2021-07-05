a9b9419a50f2a87d8193c56e3117fe6bd7487f9a
George <grepole121@gmail.com>
2021-07-05 11:13:19 +0100

    Adding empty project

---
+++ src/test/java/com/sparta/eng87/spartaglobal_vhswebsite/SpartaGlobalVhsWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.eng87.spartaglobal_vhswebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaGlobalVhsWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
