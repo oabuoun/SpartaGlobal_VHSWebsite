42578be8c54cf7a82c5596dd7b3a0f20e363d020
AlexBL <alexbarberlynch@gmail.com>
2021-07-06 10:59:18 +0100

    created more SQL Queries

--- src/main/java/com/sparta/eng87/spartaglobal_vhswebsite/repositories/SearchbarRepository.java
+++ src/main/java/com/sparta/eng87/spartaglobal_vhswebsite/repositories/SearchbarRepository.java
@@ -7,6 +7,21 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SearchbarRepository extends JpaRepository<FilmEntity, Integer> {
 
-    @Query(value = "SELECT f.* FROM film f WHERE f.title = ?", nativeQuery = true)
+    @Query(value = "SELECT f.* FROM film f WHERE f.title LIKE %?1%;", nativeQuery = true)
     List<FilmEntity> findFilmsFromTitle (String title);
+
+    @Query(value = "SELECT f.* FROM film f " +
+            "INNER JOIN film_actor fa ON f.film_id = fa.film_id" +
+            "INNER JOIN actor a ON fa.actor_id = a.actor_id" +
+            "WHERE a.first_name LIKE %?1% OR a.last_name LIKE %?1%", nativeQuery = true)
+    List<FilmEntity> findFilmsFromActor (String name);
+
+    @Query(value = "SELECT f.* FROM film f " +
+            "INNER JOIN film_actor fa ON f.film_id = fa.film_id" +
+            "INNER JOIN actor a ON fa.actor_id = a.actor_id" +
+            "WHERE a.first_name LIKE %?1% AND a.last_name LIKE %?2%", nativeQuery = true)
+    List<FilmEntity> findFilmsFromActor (String firstName, String lastName);
+
+    @Query(value = "SELECT f.* FROM film f WHERE f.genre = ?", nativeQuery = true)
+    List<FilmEntity> findFilmsFromGenre (String genre);
 }
