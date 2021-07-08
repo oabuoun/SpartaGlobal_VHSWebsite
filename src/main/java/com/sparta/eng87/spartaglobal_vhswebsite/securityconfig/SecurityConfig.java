3a1a02f8d53c0e869f032669b44e09a1cbb855ee
AlexanderLegon <alexlegon2014@gmail.com>
2021-07-08 10:23:15 +0100

    Added Logout Feature

--- src/main/java/com/sparta/eng87/spartaglobal_vhswebsite/securityconfig/SecurityConfig.java
+++ src/main/java/com/sparta/eng87/spartaglobal_vhswebsite/securityconfig/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
 import javax.sql.DataSource;
 
@@ -37,8 +38,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin/*").hasAuthority("EMPLOYEE")
                 .antMatchers("/user/*").hasAuthority("CUSTOMER")
                 .anyRequest().permitAll()
-                .and().formLogin().loginPage("/login").permitAll().successHandler(myAuthenticationSuccessHandler());
-
+                .and().formLogin().loginPage("/login").permitAll().successHandler(myAuthenticationSuccessHandler())
+        .and().logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .deleteCookies()
+        );
     }
 
 
