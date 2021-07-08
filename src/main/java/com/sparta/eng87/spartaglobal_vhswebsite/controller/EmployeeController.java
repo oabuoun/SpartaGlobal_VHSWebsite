908a98c5a5ea0468af0f496bc077a0079ee071da
AlexBL <alexbarberlynch@gmail.com>
2021-07-08 11:59:23 +0100

    added features to the Employee page

--- src/main/java/com/sparta/eng87/spartaglobal_vhswebsite/controller/EmployeeController.java
+++ src/main/java/com/sparta/eng87/spartaglobal_vhswebsite/controller/EmployeeController.java
@@ -2,13 +2,18 @@ package com.sparta.eng87.spartaglobal_vhswebsite.controller;
 
 import com.sparta.eng87.spartaglobal_vhswebsite.entities.CustomerEntity;
 import com.sparta.eng87.spartaglobal_vhswebsite.entities.FilmEntity;
+import com.sparta.eng87.spartaglobal_vhswebsite.entities.LoginEntity;
 import com.sparta.eng87.spartaglobal_vhswebsite.services.CustomerService;
 import com.sparta.eng87.spartaglobal_vhswebsite.services.FilmService;
+import com.sparta.eng87.spartaglobal_vhswebsite.services.LoginService;
+import com.sparta.eng87.spartaglobal_vhswebsite.services.StockCheckerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EmployeeController {
@@ -18,11 +23,15 @@ public class EmployeeController {
 
     private FilmService filmService;
     private CustomerService customerService;
+    private StockCheckerService stockCheckerService;
+    private LoginService loginService;
 
     @Autowired
-    public EmployeeController(FilmService filmService, CustomerService customerService){
+    public EmployeeController(FilmService filmService, CustomerService customerService, StockCheckerService stockCheckerService, LoginService loginService){
         this.filmService=filmService;
         this.customerService =customerService;
+        this.stockCheckerService = stockCheckerService;
+        this.loginService = loginService;
     }
 
     @GetMapping("/employee")
@@ -36,11 +45,28 @@ public class EmployeeController {
     }
 
     @PostMapping("/addCustomer")
-    public void addCustomer(CustomerEntity customerEntity, Model model){
-        customerService.save(customerEntity);
+    public String addCustomer(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password, Model model){
+        LoginEntity loginEntity = new LoginEntity();
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        loginEntity.setUserName(username);
+        loginEntity.setUserPassword(encoder.encode(password));
+        loginEntity.setUserRole("USER");
+        loginEntity.setUserActivated(1);
+        loginService.addUser(loginEntity);
+        return "redirect:/employee";
     }
 
+    @GetMapping("/addUser")
+    public String getAddCustomerPage() {
+        return "addUser";
+    }
 
+    @PostMapping("/employeeSearch")
+    public String employeeSearch (@RequestParam(name = "search") String search, Model model) {
+        model.addAttribute("inStock",stockCheckerService.isInStock(filmService.findFilmsByTitle(search)));
+        model.addAttribute("films", filmService.findFilmsByTitle(search));
+        return "employeeResults";
+    }
 
 
 }
