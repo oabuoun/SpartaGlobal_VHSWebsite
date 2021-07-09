022a6c886c468d8795108720cc80172cb8e1bc24
Tom <T-Canfield@virginmedia.com>
2021-07-09 11:14:20 +0100

    fixing carosel and home page rent bugs

--- src/main/java/com/sparta/eng87/spartaglobal_vhswebsite/controller/RentalController.java
+++ src/main/java/com/sparta/eng87/spartaglobal_vhswebsite/controller/RentalController.java
@@ -2,11 +2,11 @@ package com.sparta.eng87.spartaglobal_vhswebsite.controller;
 
 import com.sparta.eng87.spartaglobal_vhswebsite.entities.FilmEntity;
 import com.sparta.eng87.spartaglobal_vhswebsite.entities.RentalEntity;
-import com.sparta.eng87.spartaglobal_vhswebsite.services.FilmService;
-import com.sparta.eng87.spartaglobal_vhswebsite.services.RentalService;
+import com.sparta.eng87.spartaglobal_vhswebsite.repositories.LoginRepository;
+import com.sparta.eng87.spartaglobal_vhswebsite.services.*;
 
-import com.sparta.eng87.spartaglobal_vhswebsite.services.StockCheckerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,23 +23,34 @@ public class RentalController {
     private RentalService rentalService;
     private FilmService filmService;
     private StockCheckerService stockCheckerService;
-
-    public RentalController(RentalService rentalService, FilmService filmService,StockCheckerService stockCheckerService) {
+    private LoginRepository loginRepository;
+    private HomePageFilmsService homePageFilmsService;
+    public RentalController(RentalService rentalService, FilmService filmService,StockCheckerService stockCheckerService, LoginRepository loginRepository, HomePageFilmsService homePageFilmsService) {
         this.filmService = filmService;
         this.rentalService = rentalService;
         this.stockCheckerService = stockCheckerService;
+        this.loginRepository = loginRepository;
+        this.homePageFilmsService = homePageFilmsService;
     }
 
     @PostMapping("/rent/{id}")
-    public String saveRental (@PathVariable("id") Integer id , Principal principal) {
+    public String saveRental (@PathVariable("id") Integer id , Principal principal, Model model) {
+
         if (principal == null) {
             return "redirect:/login";
         }
-        List<FilmEntity> film = new ArrayList<>();
-        film.add(filmService.findFilmByID(id));
-        Integer inventoryID = stockCheckerService.getStock(film);
-        rentalService.saveRental(inventoryID, principal);
+        if(loginRepository.findCustomerIdByUsername(principal.getName())!=null) {
+            List<FilmEntity> film = new ArrayList<>();
+            film.add(filmService.findFilmByID(id));
+            Integer inventoryID = stockCheckerService.getStock(film);
+            rentalService.saveRental(inventoryID, principal);
+            model.addAttribute("error",false);
+        }else {
+            model.addAttribute("error",true);}
 
-        return "redirect:/";
+        model.addAttribute("recentVHS", homePageFilmsService.getRecentlyUpdated());
+        model.addAttribute("returnedVHS",homePageFilmsService.getMostRecentReturns());
+        model.addAttribute("popularVHS",homePageFilmsService.getMostPopular());
+        return "index";
     }
 }
